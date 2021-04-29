cd47bad398dccda7cc1f13a927c1b6a3fbe3e204
Zeyu Tang <zeyut@login001.palmetto.clemson.edu>
2021-04-29 13:27:17 -0400

    initialized pytorch setup

--- ins_env_2.4.sh
+++
@@ -1,7 +0,0 @@
-pip install tensorflow-gpu==2.4.0 \
-            opencv-contrib-python==4.2.0.34 \
-            opencv-python==4.2.0.34 \
-            scikit-image==0.17.1 \
-            scikit-learn==0.22.2.post1 \
-            sklearn==0.0 \
-            multiprocess==0.70.11.1
--- load_env_2.4.sh
+++
@@ -1,5 +0,0 @@
-module load cuda/11.0.3-gcc/8.3.1 
-module load cudnn/8.0.0.180-10.2-linux-x64-gcc/8.3.1 
-module load anaconda3/2019.10-gcc/8.3.1
-module load ffmpeg/4.2.2-gcc/8.3.1 
-source activate tf-2.4
